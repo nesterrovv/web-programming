b15da79535ea44c16186e0180d7f7dfc148578e3
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-02-18 20:33:10 +0300

    added js functions for number validation

--- lab1/validator.js
+++ lab1/validator.js
@@ -7,6 +7,22 @@ function receiveDataFromForm() {
     return [x, y, r];
 }
 
+function isNumber(number) {
+    if (number != null && number != "") {
+        if (/^(0$|-?[1-9]\d*(\.\d*[1-9]$)?|-?0\.\d*[1-9])$/.test(number)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isCorrectNumber(number) {
+    if (number > -5 && number < 3) {
+        return true;
+    }
+    return false;
+}
+
 function validateData() {
     // TODO: validation logic
 }
