72993f56a80c34c722d08c58581e96fe9acc8a4d
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-02-19 17:50:53 +0300

    fixed bugs in backendValidator.php

--- lab1/phpScripts/backendValidator.php
+++ lab1/phpScripts/backendValidator.php
@@ -2,10 +2,10 @@
 
 /**
  * The module for data validation on back-end side of application
- * @param $x value of x variable from html-form
- * @param $y value of y variable from html-form
- * @param $r value of R parameter from html-form
- * @return bool result of validation
+ * @param $x - value of x variable from html-form
+ * @param $y - value of y variable from html-form
+ * @param $r - value of R parameter from html-form
+ * @return bool - result of validation
  */
 function validate($x, $y, $r){
     if (!is_double($x) || !is_int($x) || !is_double($y) || !is_int($y) || !is_int($r)){
@@ -17,7 +17,7 @@ function validate($x, $y, $r){
     if ($y <= -5 || $y >= 3) {
         return false;
     }
-    if (!($r != 1) || !(r != 2) || !(r != 3) || !(r != 4) || !(r != 5)) {
+    if (!($r != 1) || !($r != 2) || !($r != 3) || !($r != 4) || !($r != 5)) {
         return false;
     }
     return true;
