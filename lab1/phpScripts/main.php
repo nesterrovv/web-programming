b10c42e9219c0a7e7087eef2e33996c0a712d995
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-02-19 18:20:09 +0300

    added main php script on back-end part of app

---
+++ lab1/phpScripts/main.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "backendValidator.php";
+require_once "areaChecker.php";
+
+date_default_timezone_set('Europe/Moscow');
+
+$start = microtime(true);
+$current_time = date("H:i:s");
+
+if (isset($_POST["x"]) && isset($_POST["y"]) && isset($_POST["r"])) {
+    $x = $_POST["x"];
+    $y = $_POST["y"];
+    $r = $_POST["r"];
+    if (validate()) {
+        $hit_result = pointInArea($x, $y, $r) ? "<span style='color: #0fc40f'>TRUE</span>" : "<span style='color: red'>FALSE</span>";
+        $execution_time = number_format(microtime(true) - $start, 8, ".", "") * 1000000;
+        die(<<<_END
+        <tr>
+            <th style="max-width: 300px; word-wrap: break-word";>$x</th>
+            <th>$y</th>
+            <th>$r</th>
+            <th>$current_time</th>
+            <th>$execution_time</th>
+            <th>$hit_result</th>
+        </tr>
+_END
+        );
+    }
+}
+die("Data is incorrect! Try again!");
