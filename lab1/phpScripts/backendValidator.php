bcddd228355d36921bf54e2c87c69f12bdd80428
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-02-19 17:16:48 +0300

    added back-end part of data validation

---
+++ lab1/phpScripts/backendValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Function for data validation on back-end side of application
+ * @param $x value of x variable from html-form
+ * @param $y value of y variable from html-form
+ * @param $r value of R parameter from html-form
+ * @return bool result of validation
+ */
+function validate($x, $y, $r){
+    if (!is_double($x) || !is_int($x) || !is_double($y) || !is_int($y) || !is_int($r)){
+        return false;
+    }
+    if ($x <= -5 || $x >= 3) {
+        return false;
+    }
+    if ($y <= -5 || $y >= 3) {
+        return false;
+    }
+    if (!($r != 1) || !(r != 2) || !(r != 3) || !(r != 4) || !(r != 5)) {
+        return false;
+    }
+    return true;
+}
